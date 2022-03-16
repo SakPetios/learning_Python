7f771889751d77633c38b707c85303bba7a079c6
Antonis <petsosantonios@gmail.com>
2022-03-16 22:51:21 +0200

    SETS.py

---
+++ SETS.py
@@ -0,0 +1,8 @@
+# I was testing Git
+my_set = {
+    "Hello world": 'HELLO WORLD',
+    "What's your name?": 'My name is antonis!',
+    "how old are you?": 'I am 15 years old'
+}
+print(my_set["What's your name?"])
+print(my_set["how old are you?"])
